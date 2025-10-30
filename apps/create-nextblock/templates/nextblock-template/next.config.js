e58a6fe0427b803367156f7ad8893109febda988
Nicolas Desjardins <nick.desjardins@live.com>
2025-10-30 14:58:10 -0400

    removed the quality warnings for images

--- apps/create-nextblock/templates/nextblock-template/next.config.js
+++ apps/create-nextblock/templates/nextblock-template/next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
     deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1440, 1920, 2048, 2560],
+    qualities: [60, 75],
     minimumCacheTTL: 31_536_000,
     dangerouslyAllowSVG: false,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
